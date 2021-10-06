457ade37095a991182d7a550a1655ca673c2039a
krasnovdnl@gmail.com <krasnovdnl@gmail.com>
2021-10-06 22:45:53 +0300

    first blood

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+/* 
+Все что находится тут  - комент
+*/
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/logotype.png') {
+        myImage.setAttribute ('src','images/firefox.png');
+    } else {
+        myImage.setAttribute('src', 'images/logotype.png')
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is boolean sheat, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is boolean sheat, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
